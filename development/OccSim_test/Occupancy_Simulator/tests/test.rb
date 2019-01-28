9ebfb731367cff7e3a7dba3dd763c04f385b465b
tsbyq <hanlibpd@gmail.com>
2019-01-28 10:46:28 -0800

    Updates

--- development/OccSim_test/Occupancy_Simulator/tests/test.rb
+++ development/OccSim_test/Occupancy_Simulator/tests/test.rb
@@ -1,3 +1,21 @@
+# *** Copyright Notice ***
+
+# OS Measures Copyright (c) 2018, The Regents of the University of California, 
+# through Lawrence Berkeley National Laboratory (subject to receipt of any required 
+#   approvals from the U.S. Dept. of Energy). All rights reserved.
+
+# If you have questions about your rights to use or distribute this software, 
+# please contact Berkeley Lab's Innovation & Partnerships Office at  IPO@lbl.gov.
+
+# NOTICE.  This Software was developed under funding from the U.S. Department of 
+# Energy and the U.S. Government consequently retains certain rights. As such, 
+# the U.S. Government has been granted for itself and others acting on its behalf 
+# a paid-up, nonexclusive, irrevocable, worldwide license in the Software to 
+# reproduce, distribute copies to the public, prepare derivative works, and 
+# perform publicly and display publicly, and to permit other to do so. 
+
+# ****************************
+
 require 'openstudio' # Note: The measure is available with OpenStuio >= 2.7.0
 require 'openstudio/ruleset/ShowRunnerOutput'
 require 'minitest/autorun'
@@ -25,7 +43,7 @@ class OccupancySimulatorTest < Minitest::Test
     osm_directory = path
     create_model(building_type, vintage, climate_zone, osm_directory)
 
-    # Move the mocdel to the test main folder
+    # Move the model to the test main folder
     FileUtils.mv(path + '/SR1/in.osm', path + '/test_model.osm')
     FileUtils.mv(path + '/SR1/in.idf', path + '/test_model.idf')
     FileUtils.mv(path + '/SR1/in.epw', path + '/test_model.epw')
